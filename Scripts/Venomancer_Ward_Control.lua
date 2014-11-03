9f45c818a4c0349f6b19b3f08cd4a6de36f8b072
edwynxero <edwynxero@gmail.com>
2014-11-03 11:35:13 +0530

    Comments included!

--- Scripts/Venomancer_Ward_Control.lua
+++ Scripts/Venomancer_Ward_Control.lua
@@ -1,34 +1,52 @@
+--<< (Deny / Last Hit) Creeps | Slow Enemies with Plague Wards >>
+
+--===================--
+--     LIBRARIES     --
+--===================--
 require("libs.ScriptConfig")
 require("libs.Utils")
 
+--===================--
+--      CONFIG       --
+--===================--
 config = ScriptConfig.new()
 config:SetParameter("SlowEnemies", true)
 config:SetParameter("DenyWithWards", true)
 config:SetParameter("LastHitWithWards", true)
 config:Load()
 
-local slowEnemy = config.SlowEnemies
-local wardLastHit = config.LastHitWithWards
-local wardDeny = config.DenyWithWards
+local slowEnemy 	= config.SlowEnemies
+local wardLastHit 	= config.LastHitWithWards
+local wardDeny 		= config.DenyWithWards
 
-damage = {10,19,29,38}
-damage[0] = 0
+--===================--
+--       CODE        --
+--===================--
+damage 		= {10,19,29,38}
+damage[0] 	= 0
 
 function Tick(tick)
-
-	if not client.connected or client.loading or client.console or not SleepCheck() then return end
-
+	if not client.connected or client.loading or client.console or (sleepTick and sleepTick > tick) or not activated then
+		--"Script Not Activated!"
+		return
+	end
+	
 	local me = entityList:GetMyHero()   
 	if not me then return end Sleep(125)
 		
 	if me.classId ~= CDOTA_Unit_Hero_Venomancer then
+		--"Script Disabled!"
 		script:Disable()
 	else
+		-- Get Hero Abilities
 		local abilityName = me:FindSpell("venomancer_plague_ward")
 		
-		local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,visible = true, alive = true, team = me:GetEnemyTeam(),illusion=false})
-		local creeps = entityList:GetEntities({classId=CDOTA_BaseNPC_Creep_Lane,alive=true,visible=true})
-		local ward = entityList:GetEntities({classId=CDOTA_BaseNPC_Venomancer_PlagueWard,alive = true,visible = true,controllable=true})
+		-- Get Visible Enemies in range
+		local enemies 	= entityList:GetEntities({type=LuaEntity.TYPE_HERO,visible = true, alive = true, team = me:GetEnemyTeam(),illusion=false})
+		-- Get Creeps in range
+		local creeps 	= entityList:GetEntities({classId=CDOTA_BaseNPC_Creep_Lane,alive=true,visible=true})
+		-- Get Plague Wards in range
+		local ward 		= entityList:GetEntities({classId=CDOTA_BaseNPC_Venomancer_PlagueWard,alive = true,visible = true,controllable=true})
 		
 		for i,v in ipairs(enemies) do
 			if not v:DoesHaveModifier("modifier_venomancer_poison_sting_ward") and v.health > 0 and slowEnemy then
