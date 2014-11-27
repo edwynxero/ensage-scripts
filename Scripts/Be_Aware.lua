5b063114e9ecb2c5115038c1f5ae809baf3aa736
edwynxero <edwynxero@gmail.com>
2014-11-27 23:09:05 +0530

    Update Be_Aware.lua

--- Scripts/Be_Aware.lua
+++ Scripts/Be_Aware.lua
@@ -1,9 +1,9 @@
---<<Messages if gank chances | Version: 1.0>>
+--<<Messages if gank chances | Version: 1.1>>
 --[[
 	------------------------------------------
 	| Be Aware of Skills Script by edwynxero |
 	------------------------------------------
-	=============== Version 1.0 ==============
+	=============== Version 1.1 ==============
 
 	Description:
 	------------
@@ -11,7 +11,6 @@
 
 		Currently Includes:
 			- Mirana's Moonlight Shadow
-			- Spirit Breaker's Charge
 
 		To-Do:
 			- Much More....
@@ -26,23 +25,16 @@ require("libs.ScriptConfig")
 --CONFIG
 config = ScriptConfig.new()
 config:SetParameter("detectMirana", true)
-config:SetParameter("detectSpiritCharge", true)
 config:Load()
 
 --SETTINGS
-local detect_Mirana        = config.detectMirana
-local detect_Spirit_Charge = config.detectSpiritCharge
+local detect_Mirana = config.detectMirana
 
 --CODE
 local registered = nil
 
---[[            Spirit Breaker          ]]
-	local mode_bara = false
-	local isCharged = false
-
 --[[                Mirana              ]]
-	local mode_mirana      = false
-	local isMiranaUltimate = false
+	local isMoonlightCasted = false
 
 --[[Loading Script...]]
 function onLoad()
@@ -61,65 +53,41 @@ end
 function Main(tick)
 	if not SleepCheck() then return end
 	local me = entityList:GetMyHero() if not me then return end
+	local team = me.team
 
-	-- Get visible enemies / teammates --
-	local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO, visible = true, alive = true, team = me:GetEnemyTeam()})
-	local teamies = entityList:GetEntities({type=LuaEntity.TYPE_HERO, visible = true, alive = true, team = me.team})
+	-- Get visible cast & heroes --
+	local cast    = entityList:GetEntities({classId=CDOTA_BaseNPC})
+	local heroes  = entityList:GetEntities({type=LuaEntity.TYPE_HERO})
 
-
-	for i,v in ipairs(enemies) do
-		if v.classId == CDOTA_Unit_Hero_Mirana and detect_Mirana then checkMirana(enemies) end
-		if v.classId == CDOTA_Unit_Hero_SpiritBreaker and detect_Spirit_Charge then checkCharge(teamies) end
+	for i,v in ipairs(heroes) do
+		if v.team ~= team and not v:IsIllusion() then
+			if v.classId == CDOTA_Unit_Hero_Mirana then MoonlightShadow(heroes, team) end
+		end
 	end
 end
 
-function checkCharge(players)
+function MoonlightShadow(heroes, team)
 	local target = nil
-	for i,v in ipairs(players) do
-		if mode_bara then
-			if v.visible == true and not v:DoesHaveModifier("modifier_spirit_breaker_charge_of_darkness_vision") then
-				mode_bara = false
-			end
-		elseif not isCharged then
-			if v:DoesHaveModifier("modifier_spirit_breaker_charge_of_darkness_vision") then
+	for i,v in ipairs(heroes) do
+		if v.team ~= team and v.visible and v.alive then
+			if isMoonlightCasted and not v:DoesHaveModifier("modifier_mirana_moonlight_shadow") then
+				isMoonlightCasted = not isMoonlightCasted
+			elseif not isMoonlightCasted and v:DoesHaveModifier("modifier_mirana_moonlight_shadow") then
 				target = v
-				isCharged = true
-				mode_bara = true
+				isMoonlightCasted = not isMoonlightCasted
 			end
 		end
 	end
-
-	if isCharged and target then
-		GenerateSideMessage(target.name:gsub("npc_dota_hero_",""),"spirit_breaker_charge_of_darkness")
-		isCharged = not isCharged
-	end
-end
-
-function checkMirana(players)
-	for i,v in ipairs(players) do
-		if mode_mirana then
-		 	if v.visible == true and not v:DoesHaveModifier("modifier_mirana_moonlight_shadow") then
-				mode_mirana = false
-			end
-		elseif not isMiranaUltimate then
-			if v:DoesHaveModifier("modifier_mirana_moonlight_shadow") then
-				isMiranaUltimate = true
-				mode_mirana = true
-			end
-		end
-	end
-
-	if isMiranaUltimate then
+	if target then
 		GenerateSideMessage("mirana","mirana_invis")
-		isMiranaUltimate = not isMiranaUltimate
 	end
 end
 
 function GenerateSideMessage(heroName,spellName)
-	local test = sideMessage:CreateMessage(200,60)
-	test:AddElement(drawMgr:CreateRect(10,10,72,40,0xFFFFFFFF,drawMgr:GetTextureId("NyanUI/heroes_horizontal/"..heroName)))
-	test:AddElement(drawMgr:CreateRect(85,16,62,31,0xFFFFFFFF,drawMgr:GetTextureId("NyanUI/other/arrow_usual")))
-	test:AddElement(drawMgr:CreateRect(150,11,40,40,0xFFFFFFFF,drawMgr:GetTextureId("NyanUI/spellicons/"..spellName)))
+	local test = sideMessage:CreateMessage(180,50)
+	test:AddElement(drawMgr:CreateRect(10,10,54,30,0xFFFFFFFF,drawMgr:GetTextureId("NyanUI/heroes_horizontal/"..heroName)))
+	test:AddElement(drawMgr:CreateRect(70,12,62,31,0xFFFFFFFF,drawMgr:GetTextureId("NyanUI/other/arrow_usual")))
+	test:AddElement(drawMgr:CreateRect(140,10,30,30,0xFFFFFFFF,drawMgr:GetTextureId("NyanUI/spellicons/"..spellName)))
 end
 
 function onClose()
